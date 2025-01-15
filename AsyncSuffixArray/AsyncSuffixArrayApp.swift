7c6ce88108b28b100a7d7fb153401ba06f6a7c5c
shfyr <lizavetaprokudina@gmail.com>
2025-01-15 18:48:24 +0300

    Initial Commit

---
+++ AsyncSuffixArray/AsyncSuffixArrayApp.swift
@@ -0,0 +1,17 @@
+//
+//  AsyncSuffixArrayApp.swift
+//  AsyncSuffixArray
+//
+//  Created by Elizaveta on 15.01.2025.
+//
+
+import SwiftUI
+
+@main
+struct AsyncSuffixArrayApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
